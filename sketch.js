782d6d4508bab77747e017de663fbba1d756ee3b
Beshadhi <138284197+Beshadhi@users.noreply.github.com>
2023-07-01 15:03:22 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -74,7 +74,7 @@ function gotResult(error, results) {
   label = results[0].label;
 
   // play sound
-  if (label == 'obstacles') {
+  if (label == 'Obstacles') {
     counter++;
     // Play sound if "helmet" is continuously predicted for 5 times
     if (counter >= 2) {
